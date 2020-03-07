d0e09f28b95941d9e9407a744af40a38a6cedac4
mathilde <ceccaroli.m@gmail.com>
2020-03-07 16:32:19 +0100

    Adding winner & end of game condition

--- React/tic-tac-toe-tuto/src/index.js
+++ React/tic-tac-toe-tuto/src/index.js
@@ -17,15 +17,44 @@ class Board extends React.PureComponent {
         this.state = {
             squares: Array(9).fill(null),
             xPlaysNext: true,
+            won: false,
         }
     }
 
+    checkWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+          ];
+          for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] 
+                && squares[a] === squares[b] 
+                && squares[a] === squares[c]) {
+              return squares[a];
+            }
+          }
+          return false;
+    }
+
     handleClick(i) {
+        if (this.state.won !== false
+            || this.state.squares[i]){
+            return;
+        }
+
         const squares = this.state.squares.slice();
         squares[i] = this.state.xPlaysNext ? 'X' : 'O';
         this.setState({
             squares: squares,
             xPlaysNext: !this.state.xPlaysNext,
+            won: this.checkWinner(squares),
         });
     }
 
@@ -35,7 +64,9 @@ class Board extends React.PureComponent {
     }
 
     render (){
-        const status = `Next player: ${(this.state.xPlaysNext ? 'X' : 'O')}`;
+        const status = this.state.won !== false
+        ? 'Winner: ' + this.state.won
+        : `Next player: ${(this.state.xPlaysNext ? 'X' : 'O')}`;
 
         return (
             <div>
